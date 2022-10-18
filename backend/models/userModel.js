8787a1f0be6cf4dd4f734a52844ec2d7787e6710
Laari_Stephen <lastepcode@gmail.com>
2022-10-18 15:36:34 -0700

    Created user signup and login authentication backend with validator & jsonwebtoken

---
+++ backend/models/userModel.js
@@ -0,0 +1,67 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
+const validator = require("validator");
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+});
+
+// static dignup method
+
+userSchema.statics.signup = async function (email, password) {
+  if (!email || !password) {
+    throw Error("All Fields Are Required");
+  }
+  if (!validator.isEmail(email)) {
+    throw Error("Invalid Email");
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw Error(
+      "Password should contain uppercase, lowercase, numbers and special character "
+    );
+  }
+
+  const exists = await this.findOne({ email });
+  if (exists) {
+    throw Error("user already in use");
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+  const user = await this.create({ email, password: hash });
+
+  return user;
+};
+
+// static login method
+
+userSchema.statics.login = async function (email, password) {
+  if (!email || !password) {
+    throw Error("All Fields Are Required");
+  }
+
+  const user = await this.findOne({ email });
+
+  if (!user) {
+    throw Error("Invalid Email");
+  }
+
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) {
+    throw Error("Invalid Password");
+  }
+
+  return user;
+};
+
+module.exports = mongoose.model("user", userSchema);
--- backend/server.js
+++ backend/server.js
@@ -3,22 +3,15 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const cors = require("cors");
 const workoutRoute = require("./api/routes/workoutRoute");
+const userRoute = require("./api/routes/userRoute");
 const app = express();
 
 //middlewares
 app.use(cors());
 
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:5000");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
-
 app.use(express.json());
 app.use("/api/workouts", workoutRoute);
+app.use("/api/user", userRoute);
 
 function start() {
   try {
